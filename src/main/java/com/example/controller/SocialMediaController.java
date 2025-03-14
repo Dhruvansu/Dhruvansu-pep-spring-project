a7906abc7a58e13fc891530dfa6fafd962c76400
Dhruvansu Thakkar <dt582675@wne.edu>
2025-03-14 22:12:41 +0000

    Feat: Implemented Message creating, retrieval, retrieval using UserId, patching, and deletion successfully

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,11 +1,17 @@
 package com.example.controller;
 
+import java.util.List;
+
 import javax.naming.AuthenticationException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -65,4 +71,43 @@ public class SocialMediaController {
         return exception.getMessage();
     }
 
+    @PostMapping("messages")
+    public ResponseEntity<Message> postMessage(@RequestBody Message message) throws Exception{
+        Message postedMessage = messageService.postMessage(message.getPostedBy(), 
+                                                        message.getMessageText(), 
+                                                        message.getTimePostedEpoch());
+        return ResponseEntity.ok().body(postedMessage);
+    }
+
+    @GetMapping("messages")
+    public ResponseEntity<List<Message>> getAllMessages(){
+        List<Message> allMessages = messageService.getAllMessages();
+        return ResponseEntity.ok().body(allMessages);
+    }
+
+    @GetMapping("messages/{messageId}")
+    public ResponseEntity<Message> getMessageUsingMessageId(@PathVariable Integer messageId){
+        Message retrievedMessage = messageService.getMessageUsingMessageId(messageId);
+        return ResponseEntity.ok().body(retrievedMessage);
+    }
+
+    @DeleteMapping("messages/{messageId}")
+    public ResponseEntity<String> deleteMessageUsingMessageId(@PathVariable Integer messageId){
+        Boolean messageDeletedSuccessfully = messageService.deleteMessageUsingMessageId(messageId);
+        return messageDeletedSuccessfully ? 
+            ResponseEntity.ok().body("1") : ResponseEntity.ok().body("");
+    }
+
+    @PatchMapping("messages/{messageId}")
+    public ResponseEntity<String> updateMessageText(@PathVariable Integer messageId, 
+                                @RequestBody Message patchMessage) throws Exception{
+        messageService.updateMessageText(messageId, patchMessage.getMessageText());
+        return ResponseEntity.ok().body("1");
+    }
+
+    @GetMapping("accounts/{accountId}/messages")
+    public ResponseEntity<List<Message>> getAllMessageUsingAccountId(@PathVariable Integer accountId){
+        List<Message> allMessagesFromUser = messageService.getAllMessageUsingAccountId(accountId);
+        return ResponseEntity.ok().body(allMessagesFromUser);
+    }
 }
--- src/main/java/com/example/service/MessageService.java
+++ src/main/java/com/example/service/MessageService.java
@@ -1,10 +1,64 @@
 package com.example.service;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.example.entity.Message;
+import com.example.repository.AccountRepository;
+import com.example.repository.MessageRepository;
 
 @Service
 @Transactional
 public class MessageService {
+    private MessageRepository messageRepository;
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public MessageService(MessageRepository messageRepository, AccountRepository accountRepository){
+        this.messageRepository = messageRepository;
+        this.accountRepository = accountRepository;
+    }
+
+    public Message postMessage(Integer postedBy, String messageBody, Long postedTime) throws Exception{
+        if(!(messageBody.isBlank()) && (messageBody.length() < 256) &&
+             !(accountRepository.findById(postedBy).isEmpty())){
+            Message newMessage = new Message(postedBy, messageBody, postedTime);
+            messageRepository.save(newMessage);
+            return messageRepository.getById(newMessage.getMessageId());
+        }
+        throw new Exception();
+    }
+
+    public List<Message> getAllMessages(){
+        return messageRepository.findAll();
+    }
+
+    public Message getMessageUsingMessageId(Integer messageId){
+        return messageRepository.findById(messageId).orElse(null);
+    }
+
+    public Boolean deleteMessageUsingMessageId(Integer messageId){
+        if(!(messageRepository.findById(messageId).isEmpty())){
+            messageRepository.deleteById(messageId);
+            return true;
+        }
+        return false;
+    }
+
+    public void updateMessageText(Integer messageId, String messageText) throws Exception{
+        Message message = messageRepository.findById(messageId).orElseThrow();
+        if(!(messageText.isEmpty()) && (messageText.length() < 256)){
+            message.setMessageText(messageText);
+            messageRepository.save(message);
+        } else
+            throw new Exception();
+    }
+
+    public List<Message> getAllMessageUsingAccountId(Integer accountId){
+        return messageRepository.findByPostedBy(accountId).get();
+    }
 }
